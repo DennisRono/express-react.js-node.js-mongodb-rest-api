1ad23d483b8177b39ac1aff4b9b3d5d279d598c8
unknown <finnneron1@gmail.com>
2022-01-24 17:41:35 +0300

    frontend works

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,61 @@
 const express = require("express");
-
 const router = express.Router();
+const Post = require("../models/Post");
 
-router.get('/', (req, res) => {
-    res.send("we are on posts");
+//FETCHES ALL THE POSTS
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err) {
+        res.json({ message: err });
+    }
 });
 
+//FIND A SPECIFIC POST
+router.get("/:postId", async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    }catch(err){
+        res.json({ message: err });
+    }
+});
+
+//SUBMITS A POST
+router.post("/", async(req, res) => {
+    const post = new Post({
+        title: req.body.title,  
+        description: req.body.description
+    });
+    try {
+        const savedpost = await post.save()
+        res.json(savedpost)
+    } catch (err) {
+        res.json({ message: err });
+    }
+
+});
+
+//DELETE A SPECIFIC POST
+router.delete("/:postId", async (req, res) => {
+    try{
+    const delpost = await Post.remove({ _id: req.params.postId });
+    res.json(delpost);
+    } catch(err){
+        res.json({ message: err });
+    }
+});
+
+//UPDATE A POST
+router.patch("/:postId", async (req, res) => {
+    try{
+    const toupdate = await Post.updateOne({ _id: req.params.postId }, {$set: { title: req.body.title }});
+    res.json(toupdate);
+    } catch(err){
+        res.json({ message: err });
+    }
+});
 router.get('/specific', (req, res) => {
     res.send("we are on specific posts");
 });
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express()
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
+const bodyParser = require("body-parser")
 const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 app.use(cors());
+app.use(bodyParser.json());
 
 //conect to mongo db
 mongoose.connect(
